fc687d61787c7b0d732535297f6c85c28d2c119b
kanika127 <kanika127@gmail.com>
2024-03-20 03:24:21 -0700

    F: login pg; B: user authentication and tables add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-mso=4p2ey(vo15a^d2n5=5b0q*4=n)lb)4vwka#cr^wq#6vvr&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.53','127.0.0.1','localhost']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
@@ -79,15 +80,18 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
+
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
         'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+        	'read_default_file': '/etc/mysql/my.cnf',
+    	},
         'NAME' : 'Passion_Project',
         'USER' : 'root',
         'PASSWORD' : 'passionNSR',
         'HOST' : 'localhost',
         'PORT' : '3306'
-
     }
 }
 
@@ -116,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/New_York"
 
 USE_I18N = True
 
@@ -127,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
